f8b4731a6d60aa8b91dd93af40020dc23d65e117
Adam Robinson <a.robinson@searchmetrics.com>
2017-04-24 16:05:07 +0200

    feat: added a simple application, config and REST resource class

---
+++ src/main/java/com/searchmetrics/simpleEmailService/Config.java
@@ -0,0 +1,9 @@
+package com.searchmetrics.simpleEmailService;
+
+import io.dropwizard.Configuration;
+
+/**
+ *
+ */
+public class Config extends Configuration {
+}
---
+++ src/main/java/com/searchmetrics/simpleEmailService/api/rest/SimpleEmailServiceResource.java
@@ -0,0 +1,52 @@
+package com.searchmetrics.simpleEmailService.api.rest;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
+
+/**
+ *
+ */
+@Path("/")
+public class SimpleEmailServiceResource {
+
+    private final ObjectMapper OM;
+
+    public SimpleEmailServiceResource(ObjectMapper OM) {
+        this.OM = OM;
+    }
+
+    @GET
+    @Path("helloWorld")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response helloWorld(@QueryParam("userName") final Optional<String> potentialUserName) {
+            return Response.ok().entity(new HelloWorldResponse(potentialUserName, "Hello World, %s")).build();
+    }
+
+    private static class HelloWorldResponse {
+        private final String user;
+        private final String msg;
+
+        public HelloWorldResponse(Optional<String> user, String msg) {
+            this.user = user.orElseGet(() -> "F2U");
+            this.msg = String.format(msg, this.user);
+        }
+
+        @JsonProperty
+        public String getUser() {
+            return user;
+        }
+
+        @JsonProperty
+        public String getMsg() {
+            return msg;
+        }
+    }
+}
