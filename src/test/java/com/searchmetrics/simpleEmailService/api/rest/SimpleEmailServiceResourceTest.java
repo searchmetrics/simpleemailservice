0397047b93f53c808867d00ef4c8029c4a218dde
Linus Jahn <linus.jahn@searchmetrics.com>
2017-04-25 15:03:24 +0200

    Update

--- src/test/java/com/searchmetrics/simpleEmailService/api/rest/SimpleEmailServiceResourceTest.java
+++ src/test/java/com/searchmetrics/simpleEmailService/api/rest/SimpleEmailServiceResourceTest.java
@@ -5,6 +5,9 @@ import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jackson.Jackson;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -14,17 +17,17 @@ import static org.junit.Assert.*;
 public class SimpleEmailServiceResourceTest {
     SimpleEmailServiceResource serviceResource;
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
         final ObjectMapper objectMapper = Jackson.newObjectMapper();
         serviceResource = new SimpleEmailServiceResource(objectMapper);
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void helloWorld() throws Exception {
 //        final Optional<String> userName = Optional.of("");
 //        Response response = serviceResource.helloWorld(userName);
